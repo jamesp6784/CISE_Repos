948740a88dbe989ee67d5803d3e8aeea0c169c49
James Pilcher <james.pilcher@vista.co>
2024-07-19 14:31:17 +1200

    Update App.js with correct string

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello CISE
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Agile is about Values and Principless
         </a>
       </header>
     </div>
